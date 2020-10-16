eb6bdbaa64174211bc8c987aac0a400b7d42baf9
joehs16 <56398757+joehs16@users.noreply.github.com>
2020-10-16 01:04:03 -0400

    alternative BFS
    added comments. it outputs the correct array but running into strange error for testMazes

--- project_02/00_source/project2.py
+++ project_02/00_source/project2.py
@@ -11,6 +11,8 @@ Date:
 """
 import os
 os.getcwd()
+os.chdir('../../project_02/00_source')
+# os.getcwd()
 
 # Import math and other p2 files.
 import math
@@ -32,32 +34,86 @@ def bdfs(maze, alg):
     # If the alg is not BFS or DFS, raise exception.
     if (alg != 'BFS') and (alg != 'DFS'):
         raise Exception('Incorrect alg! Need BFS or DFS!')
-    # load start and exit
-    start_v = maze.start
-    exit_v = maze.exit
+
+    # # load start and exit
+    # start_v = maze.start
+    # exit_v = maze.exit
+    #
+    #
+    # for vertex in maze.adjList:
+    #     #all the vertices initalize as "not visited"
+    #     vertex.visted = False
+    #     #set previous values to none
+    #     vertex.prev = None
+    #     #set distances to infinity
+    #     vertex.dist = math.inf
+
+    """
+    BFS - Breadth First Search
+    """
     # Start BFS
     if (alg == 'BFS'):
+        #create an empty queue
         frontier = Queue()
+
+        #initialize the distance
         start_v.dist = 0
+
+        #add the starting location to the queue
         frontier.push(start_v.rank)
+
         while frontier.isEmpty()==False:
+            #look at the first vertex in the list
             current = frontier.pop()
+
+            #look at all the neighbors of the current vertex
             for neighbor in maze.adjList[current].neigh:
+                #if the vertex hasn't been visited:
                 if neighbor.dist==math.inf:
-                    print(neighbor.rank)
-                    frontier.push(neighbor.rank)
-                    #print(frontier)
+                    #mark as visited
                     neighbor.dist = maze.adjList[current].dist + 1
+                    #push the neighbor to the queue
+                    frontier.push(neighbor.rank)
                     neighbor.prev = current
+                # if neighbor.dist==math.inf:
+                #     print(neighbor.rank)
+                #     frontier.push(neighbor.rank)
+                #     #print(frontier)
+                #     neighbor.dist = maze.adjList[current].dist + 1
+                #     neighbor.prev = current
                     pass
                 pass
             pass
-        path = []
+        path = [exit_v]
         while exit_v.prev != None:
-            path.append(exit_v.prev)
+            path.insert(0,exit_v.prev)
+            #path.append(exit_v.prev)
             exit_v = maze.adjList[exit_v.prev]
+            #print(exit_v)
         return path
 
+    """
+    DFS - Depth First Search
+    This code marks each vertex that is connected to the starting vertex as 'visited'
+    """
+    # START DFS
+    #else:
+        #Step 1: Initialize
+        #EACH NODE VISITED SET TO FALSE.
+        #self.visited = False
+
+        #Step 2: Visit the first room
+        #Go to starting room
+        #Set Start.visted to True
+        #start.visit
+        #pushing to stack is visiting the room
+
+        #While the stack is not empty, pop,
+
+
+    ##### Your implementation goes here. #####
+
+    ##### Your implementation goes here. #####
 
 """
 Main function.
