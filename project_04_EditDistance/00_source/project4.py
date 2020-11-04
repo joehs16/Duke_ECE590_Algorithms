7e401643e9e49d216e863e9f783723283d5f3a62
wensu425 <s5wen@ucsd.edu>
2020-11-04 13:53:07 -0500

    dp

--- project_04_EditDistance/00_source/project4.py
+++ project_04_EditDistance/00_source/project4.py
@@ -15,10 +15,72 @@ from p4tests import *
 
 """
 ED: the edit distance function
+	dest . . .
+src
+ .
+ .
+ .
 """
 def ED(src, dest):
     dist = 0 # This is a placeholder, remove and implement!
     edits = [] # This is a placeholder, remove and implement!
+    op_names = ['insert','delete','sub','match']
+    dp_table = [[None for i in range(len(dest)+1)] for j in range(len(src)+1)]
+    # initialize base case row 0
+    for i in range(len(dest)+1):
+        dp_table[0][i] = i
+    # intialize base case column 0
+    for j in range(len(src)+1):
+        dp_table[j][0] = j
+    print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
+      for row in dp_table]))
+    for i in range(1,len(src)+1):
+        for j in range(1,len(dest)+1):
+            # insert, delete, sub_or_match
+            print(i,', ',j)
+            three_ops = [dp_table[i][j-1],dp_table[i-1][j],dp_table[i-1][j-1]]
+            edit_cost = min(three_ops)
+            edit_index = three_ops.index(edit_cost)
+            if src[i-1] == dest[j-1]:
+                dp_table[i][j] = dp_table[i-1][j-1]
+            else:
+                dp_table[i][j] = 1 + edit_cost
+    """
+    dist = dp_table[-1][-1]
+    print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
+      for row in dp_table]))
+    print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
+      for row in edit_table]))
+    """
+    # trace  back from the right bottom cell.
+    i = len(src)
+    j = len(dest)
+    while i!=0 and j!=0:
+        if src[i-1] == dest[j-1]:
+            edits.append(('match',src[i-1],i-1))
+            i-=1
+            j-=1
+        else:
+            three_ops = [dp_table[i][j-1],dp_table[i-1][j],dp_table[i-1][j-1]]
+            edit_cost = min(three_ops)
+            edit_index = three_ops.index(edit_cost)
+            if edit_index == 0:
+                edits.append((op_names[edit_index],dest[j-1],j))
+                j-=1
+            elif edit_index == 1:
+                edits.append((op_names[edit_index],dest[j-1],j))
+                i-=1
+            else:
+                edits.append((op_names[edit_index],dest[j-1],j))
+                i-=1
+                j-=1
+    while i!=0:
+        edits.append(('insert',dest[j-1],j))
+        i-=1
+    while j!=0:
+        edits.append(('delete',dest[j-1],j))
+        j-=1
+    dist=dp_table[-1][-1]
     return dist, edits
 
 ################################################################################
