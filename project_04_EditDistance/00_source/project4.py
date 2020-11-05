93e1692d0d85bb42a5b8a479aa2f0a115293c3b1
joehs16 <56398757+joehs16@users.noreply.github.com>
2020-11-05 00:32:05 -0500

    added comments

--- project_04_EditDistance/00_source/project4.py
+++ project_04_EditDistance/00_source/project4.py
@@ -14,75 +14,102 @@ from p4tests import *
 ################################################################################
 
 """
-ED: the edit distance function
-	dest . . .
-src
- .
- .
- .
+EditDistance Algorithm
+
+determines the number of edits required to convert one string into another
 """
 def ED(src, dest):
-    dist = 0 # This is a placeholder, remove and implement!
-    edits = [] # This is a placeholder, remove and implement!
-    op_names = ['insert','delete','sub','match']
+    """
+    src    # The source/starting string
+    dest   # The destination/ending string
+    """
+
+    """
+    Generation of the dynamic programming table
+    note: for the index values, i are the rows and j are the columns
+    """
+    # the initialization of the dp table.
     dp_table = [[None for i in range(len(dest)+1)] for j in range(len(src)+1)]
-    # initialize base case row 0
+    # population of the base cases for row 0.
     for i in range(len(dest)+1):
         dp_table[0][i] = i
-    # intialize base case column 0
+    # population of the base cases for column 0.
     for j in range(len(src)+1):
         dp_table[j][0] = j
+
+    # iteratively generate the DP table, starting with the base cases, moving/
+    # left to right, top to bottom.
     for i in range(1,len(src)+1):
         for j in range(1,len(dest)+1):
-            # insert, delete, sub_or_match
+            # [left/insert, down/delete, diagonal/sub_or_match]
             three_ops = [dp_table[i][j-1],dp_table[i-1][j],dp_table[i-1][j-1]]
             edit_cost = min(three_ops)
-            edit_index = three_ops.index(edit_cost)
+
+            # if match: do not update edit cost, else: update edit cost.
             if src[i-1] == dest[j-1]:
                 dp_table[i][j] = dp_table[i-1][j-1]
             else:
                 dp_table[i][j] = 1 + edit_cost
-                
+
     """
-    dist = dp_table[-1][-1]
-    print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
+    # code chunk to look at DP Table
+    print('\n'.join([''.join(['{:4}'.format(item) for item in row])
       for row in dp_table]))
-    print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
+    print('\n'.join([''.join(['{:4}'.format(item) for item in row])
       for row in edit_table]))
     """
 
-    # trace  back from the right bottom cell.
+    """
+    Using the dpTable to trace back the optimal edit operations
+    """
+    # initialize index values at the bottom right of DP table.
     i = len(src)
     j = len(dest)
-    num_insert = 0
+
+    # initialize edits list to record what edits were made.
+    edits = []
+    op_names = ['insert','delete','sub','match']
+
+    # general base case.
     while i!=0 and j!=0:
+        # b/c there two diagonal values, determine if the diagonal value is/
+        # match or substitute. if values are the same: match, else: use DP/
+        # table to determine operation.
+        # Match:
         if src[i-1] == dest[j-1]:
-            edits.append(('match',src[i-1],i-1))
             i-=1
             j-=1
+            edits.append(('match',src[i],i))
         else:
+            #evaluate which values from the dp_table gives the optimal value.
             three_ops = [dp_table[i][j-1],dp_table[i-1][j],dp_table[i-1][j-1]]
             edit_cost = min(three_ops)
             edit_index = three_ops.index(edit_cost)
-            # insert
+            # Insert:
             if edit_index == 0:
-                edits.append((op_names[edit_index],dest[j-1],i))
-                j-=1
-            # delete
+                j-= 1
+                edits.append((op_names[edit_index],dest[j],i))
+            # Delete:
             elif edit_index == 1:
-                edits.append((op_names[edit_index],src[i-1],i-1))
                 i-=1
-            # sub
+                edits.append((op_names[edit_index],src[i],i))
+            # Substitute:
             else:
-                edits.append((op_names[edit_index],dest[j-1],i-1))
                 i-=1
                 j-=1
+                edits.append((op_names[edit_index],dest[j],i))
+
+    # edge case: when in leftmost column, i.e. j = 0.
     while i!=0:
-        edits.append(('delete',src[i-1],i-1))
         i-=1
+        edits.append(('delete',src[i],i))
+
+    # edge case: when in top row, i.e. i = 0.
     while j!=0:
-        edits.append(('insert',dest[j-1],i))
         j-=1
+        edits.append(('insert',dest[j],i))
+
+    # the total edits made/distance traveled.
     dist=dp_table[-1][-1]
     return dist, edits
 
@@ -92,7 +119,7 @@ def ED(src, dest):
 Main function.
 """
 if __name__ == "__main__":
-    edTests(True)
+    edTests(False)
     print()
     compareGenomes(True, 30, 300)
     print()
